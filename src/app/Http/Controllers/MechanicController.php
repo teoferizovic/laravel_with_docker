640e080f7f66c3f08808323baa04979de928821c
teo <ferizovic.teo@gmail.com>
2020-06-01 14:12:30 +0200

    Use Events for Logging details

--- src/app/Http/Controllers/MechanicController.php
+++ src/app/Http/Controllers/MechanicController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Mechanic;
+use App\Events\ReadMechanics;
 
 class MechanicController extends Controller
 {
      public function index(Request $request, $id=null){
      	//$mechanic = Mechanic::find(1)->carOwner;
+     	//$mechanics = Mechanic::all();
+     	//$mechanics->load('car.owner');
+     	
      	$mechanics = Mechanic::with(['car.owner:id,name,car_id'])->get();
+     	
+     	event(new ReadMechanics(count($mechanics)));
      	
      	//return Mechanic::all();
      	//return $mechanics->toArray();
      	//return response()->json(['message' => 'Not Found!'], 404);
+     	
      	return \Response::json($mechanics,200);
      }
 }
